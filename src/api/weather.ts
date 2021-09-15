ad9299a7b4f9f34787b3c35172ff62899dcf5877
Luciano Dichiara <luciano@studiohakuna.com>
2021-09-14 21:55:58 -0300

    OpenWeather API connection

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,10 +1,5 @@
-const OPEN_WEATHER = `${process.env.VUE_APP_BASE_URL_OPEN_WEATHER}weather?q=Paris&units=metric&appid=${process.env.VUE_APP_API_KEY_OPEN_WEATHER}`;
-
 class WeatherService {
-
     private static bfInternal: WeatherService;
-
-    // private constructor() {}
 
     static get Instance(): WeatherService {
         if (WeatherService.bfInternal === undefined) {
@@ -12,53 +7,10 @@ class WeatherService {
         }
         return WeatherService.bfInternal;
     }
-  
-    /*
-    public getCredentials(): any {
-        const credentials = localStorage.getItem('j6session');
 
-        if (credentials) {
-        return JSON.parse(credentials);
-        }
-
-        return null;
-    }
-
-    public clearCredentials() {
-        localStorage.removeItem('j6session');
-    }
-
-    public setCredentials(credentials: any) {
-        localStorage.setItem('j6session', JSON.stringify(credentials));
-    }
-    */
-
-    
-    /** DATA READ APIS */
     public async getOpenWeatherData(): Promise<any> {
-        return this._get(OPEN_WEATHER);
+        return this._get(`${process.env.VUE_APP_BASE_URL_OPEN_WEATHER}weather?q=Rio Cuarto&units=metric&appid=${process.env.VUE_APP_API_KEY_OPEN_WEATHER}`);
     }
-
-    /*
-    public async getRunningEntries(): Promise<any> {
-        const credentials = this.getCredentials();
-
-        const response = await fetch(RUNNING_ENTRIES, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer ' + credentials.auth,
-            'Accept': 'application/json',
-        },
-        });
-
-        if (!response.ok) {
-        throw Error(response.statusText);
-        }
-
-        return response.json();
-    }
-    */
 
     private async _get(uri: string): Promise<any> {
         const response = await fetch(uri, {
--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="data-list">
-        <div class="box">
-            I'm in a box.FIRST
+        <div class="box is-flex is-justify-content-space-between" v-if="openWeather.wind">
+            <div>Open Weather</div>
+            <div>
+                {{ getFormattedSpeed(openWeather.wind.speed) }} Km/h
+                <span
+                    class="tag"
+                    :class="getSpeedAnalysis(openWeather.wind.speed).type"
+                >
+                    {{ getSpeedAnalysis(openWeather.wind.speed).status }}
+                </span>
+            </div>
         </div>
         <div class="box">
             I'm in a box.
@@ -46,8 +55,28 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class DataList extends Vue {
     @Prop(Object) private openWeather!: any;
 
-    private created() {
-        console.log(this.openWeather);
+    private getFormattedSpeed(speed: number): number {
+        const res = Math.round(speed * 1.609);
+        return res;
+    }
+
+    private getSpeedAnalysis(speed: number) {
+        let wind = this.getFormattedSpeed(speed);
+    
+        let condition = {
+            status: 'regular',
+            type: 'is-warning',
+        };
+
+        if (wind < 12) {
+            condition.status = 'good';
+            condition.type = 'is-success';
+        } else if (wind > 24) {
+            condition.status = 'bad';
+            condition.type = 'is-warning';
+        }
+
+        return condition;
     }
 }
 </script>
